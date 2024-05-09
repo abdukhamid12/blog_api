b419e48a10d21dd6bcea1818f55ca289a4e12e48
Smoke_Front_Back_Developing <116000706+abdukhamid12@users.noreply.github.com>
2024-05-09 17:43:23 +0500

    17:50

--- config/settings.py
+++ config/settings.py
@@ -138,10 +138,11 @@ LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = "index"
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'  # new
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
