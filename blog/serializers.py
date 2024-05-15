5dff496fe9d5dc3687af33d460f3c294226e6a71
Smoke_Front_Back_Developing <116000706+abdukhamid12@users.noreply.github.com>
2024-05-15 14:10:30 +0500

    14:17

--- blog/serializers.py
+++ blog/serializers.py
@@ -14,4 +14,4 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'created', 'updated', 'is_published']
+        fields = ['id', 'title', 'image', 'content', 'created', 'updated', 'is_published']
